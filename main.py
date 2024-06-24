be51d0a39b53b44e3da2617d348b549a13c404c1
dhatchayaniudhayakumar <dhatchayani8102@gmail.com>
2024-06-24 13:53:19 +0530

    comment above the loop

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # Simple Python Program to determine whether a given integer is Prime : 
 integer = 9
-if integer > 1:
-    for i in range (2,(integer//2)+1):
-        if (integer%i)==0:
+ # First its check  give integer is greater than  1
+if integer > 1:   
+    #Iterate i from 2  to  integer//2 +1                              
+    for i in range (2,(integer//2)+1):   
+        #if integer is divisible by any number of i ,it is noy prime         
+        if (integer%i)==0:                        
           print("The integer is not a prime !")
         else:
           print("The integer is  a prime !")
